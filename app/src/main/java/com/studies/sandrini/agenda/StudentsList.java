814b7e00c3a9d0819d9c87da8afe10211fcdc644
Sandrini <thesandrini@gmail.com>
2017-09-14 20:03:05 -0300

    Second commit w/ form

--- app/src/main/java/com/studies/sandrini/agenda/StudentsList.java
+++ app/src/main/java/com/studies/sandrini/agenda/StudentsList.java
@@ -2,6 +2,8 @@ package com.studies.sandrini.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StudentsList extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class StudentsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students_list);
+
+        String[] students = {"Leo", "Fer", "Math", "Nick"};
+        ListView studentsList = (ListView)findViewById(R.id.students_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
+        studentsList.setAdapter(adapter);
     }
 }
