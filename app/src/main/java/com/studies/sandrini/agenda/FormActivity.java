a5ba51f023b0b4808434bb20d5aa50060f2153eb
Sandrini <thesandrini@gmail.com>
2017-09-16 18:21:52 -0300

    Menu button added

--- app/src/main/java/com/studies/sandrini/agenda/FormActivity.java
+++ app/src/main/java/com/studies/sandrini/agenda/FormActivity.java
@@ -2,6 +2,10 @@ package com.studies.sandrini.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        Button saveButton = (Button) findViewById(R.id.form_save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormActivity.this, "Student saved", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.form_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/studies/sandrini/agenda/StudentsList.java
+++ app/src/main/java/com/studies/sandrini/agenda/StudentsList.java
@@ -1,8 +1,11 @@
 package com.studies.sandrini.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class StudentsList extends AppCompatActivity {
@@ -16,5 +19,15 @@ public class StudentsList extends AppCompatActivity {
         ListView studentsList = (ListView)findViewById(R.id.students_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
         studentsList.setAdapter(adapter);
+
+        Button newStudent = (Button) findViewById(R.id.new_student);
+        newStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentForm = new Intent(StudentsList.this, FormActivity.class);
+                startActivity(intentForm);
+            }
+        });
+        }
     }
-}
+
